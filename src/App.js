b0e9184e92fc117e7e9c4200cd4237eb1f15d9c1
Doğan Aker <m.doganaker@gmail.com>
2021-03-17 13:07:07 +0300

    Main page menu added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { Layout, Menu } from 'antd';
+import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <>
+        <Layout className='layout'>
+        <Header>
+          <div className='logo'/>
+          <Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
+            <Menu.Item key='1'><Link to='/'>To Do</Link></Menu.Item>
+            <Menu.Item key='2'><Link to='/calender'>Calender</Link></Menu.Item>
+          </Menu>
+        </Header>
+        </Layout>
+      </>
+    </Router>
   );
 }
 
