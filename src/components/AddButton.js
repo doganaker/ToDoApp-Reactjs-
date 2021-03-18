bec9b1dba0e9c26fc982b003e413e34cca816ef1
Doğan Aker <m.doganaker@gmail.com>
2021-03-18 14:22:37 +0300

    Modal added

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
 import { Modal, Button } from 'antd';
 
 function AddButton() {
+
+    const [isModalVisible, setModalVisible] = useState(false);
+
+    const showModal = () => {
+        setModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setModalVisible(false);
+    };
+
     return (
         <>
-            <Button style={{ backgroundColor: 'lightgreen', borderRadius: '10px', color: 'white' }}><strong>Add</strong></Button>
+            <Button style={{ backgroundColor: 'lightgreen', borderRadius: '10px', color: 'white' }} onClick={showModal}><strong>Add</strong></Button>
+            <Modal title="Add/Update Todo" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} >
+
+            </Modal>
         </>
     )
 }
